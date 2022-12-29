5a30804264bc2ade48bd47ca4c0162a56c0c5205
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2022-12-29 12:11:19 -0300

    feat: add tests for valida_cpf and prepare to refactor

--- valida_cpf/valida_cpf.test.ts
+++ valida_cpf/valida_cpf.test.ts
@@ -1,8 +1,17 @@
-import { ValidatorCPF } from "./valida_cpf";
+import validate from "./valida_cpf_before"
 
-test('CPF must be 9 digits', () => { 
-	const mockCPF = '98765432'
-	const cpfValidator = new ValidatorCPF()
-
-	expect(cpfValidator.isCpfValid(mockCPF)).toBe(true)
+describe('Should Validate CPFs', () => { 
+	const cpfValidator = validate
+	test('CPF 111.111.111-11 must be invalid', () => { 
+			const mockCPF = '111.111.111-11'
+			expect(cpfValidator(mockCPF)).toBe(false)
+	 })
+	test('CPF 123.456.789-99 must be invalid', () => { 
+			const mockCPF = '123.456.789-99'
+			expect(cpfValidator(mockCPF)).toBe(false)
+	 })
+	test('CPF 935.411.347-80 must be valid', () => { 
+			const mockCPF = '935.411.347-80'
+			expect(cpfValidator(mockCPF)).toBe(true)
+	 })
  })
