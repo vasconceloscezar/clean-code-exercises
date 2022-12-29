421e7bec2faf41cfbd94b02c80fddbb2f745fb44
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2022-12-29 13:36:11 -0300

    test: ensure validatorCPF works as expected

--- valida_cpf/valida_cpf.test.ts
+++ valida_cpf/valida_cpf.test.ts
@@ -1,7 +1,9 @@
-import validate from "./valida_cpf_before"
+// import validate from "./valida_cpf_before"
+import { validatorCPF } from "./valida_cpf_after"
 
 describe('Should Validate CPFs', () => { 
-	const cpfValidator = validate
+	// const cpfValidator = validate
+	const cpfValidator = validatorCPF
 	test('CPF 111.111.111-11 must be invalid', () => { 
 			const mockCPF = '111.111.111-11'
 			expect(cpfValidator(mockCPF)).toBe(false)
@@ -13,5 +15,13 @@ describe('Should Validate CPFs', () => {
 	test('CPF 935.411.347-80 must be valid', () => { 
 			const mockCPF = '935.411.347-80'
 			expect(cpfValidator(mockCPF)).toBe(true)
+	 })
+	test('CPF 111.444.777-35 must be valid', () => { 
+			const mockCPF = '111.444.777-35'
+			expect(cpfValidator(mockCPF)).toBe(true)
+	 })
+	test('CPF empty must be invalid', () => { 
+			const mockCPF = ''
+			expect(cpfValidator(mockCPF)).toBe(false)
 	 })
  })
